315c00c2ee571d1e2245a049e3bf056c2b08ac07
Ben Matare <ben.a.matare@gmail.com>
2018-01-19 05:12:44 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,7 @@
-#def square_array(array)
-  # your code here
-#  arr = []
-#  array.each { |i| arr << i ** 2}
-#  arr
-#end
 def square_array(array)
-  array.collect {|i| i ** 2}
+  # your code here
+  arr = []
+  array.each { |i| arr << i ** 2}
+  arr
 end
+
